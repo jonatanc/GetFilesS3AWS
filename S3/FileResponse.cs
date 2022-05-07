9949f71ab8eb909a8f2bde2ad14e9e76aa6ae2a1
Jonatan Cardoso Saraiva <jonatan.cardoso@hotmail.com.br>
2022-05-07 17:07:41 -0300

    Add files via upload

---
+++ S3/FileResponse.cs
@@ -0,0 +1,37 @@
+﻿using ADAIConnect.Domain.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADAIConnect.Domain.Response
+{
+    public class FileResponse
+    {
+        public List<File> Files { get; set; }
+
+        public List<SubCategory> SubCategorys { get; set; }
+        
+        public Links Links { get; set; }
+        public Meta Meta { get; set; }
+    }
+
+
+    public class File
+    {
+        public string Name { get; set; }
+
+        public string Extension { get; set; }
+
+        public string Link { get; set; }
+    }
+
+    public class SubCategory
+    {
+        public string Name { get; set; }
+
+        public List<string> AdicionalPaths { get; set; }
+
+    }
+}
