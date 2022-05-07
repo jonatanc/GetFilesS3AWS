be2564467050c78200ad3d3976b4d3f4a1bfef90
Jonatan Cardoso Saraiva <jonatan.cardoso@hotmail.com.br>
2022-05-07 17:10:23 -0300

    Update FileResponse.cs

--- S3/FileResponse.cs
+++ S3/FileResponse.cs
@@ -1,11 +1,10 @@
-﻿using ADAIConnect.Domain.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ADAIConnect.Domain.Response
+namespace Domain.Response
 {
     public class FileResponse
     {
